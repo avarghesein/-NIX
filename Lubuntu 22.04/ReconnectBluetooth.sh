b044bd342b56bdbd1552e7fbc9b915f9d37d3b6f
AV <10294917+avarghesein@users.noreply.github.com>
2023-09-05 08:46:34 +0530

    Create ReconnectBluetooth.sh

---
+++ Lubuntu 22.04/ReconnectBluetooth.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+bluetoothctl disconnect <MAC address>
+bluetoothctl remove <MAC address>
+bluetoothctl scan on &
+sleep 10
+bluetoothctl pair <MAC address>
+bluetoothctl connect <MAC address>
